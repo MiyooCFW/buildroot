8bd4a5d2e26686c27c8d221931dded46f5f84377
tiopex <67048640+tiopex@users.noreply.github.com>
2023-03-27 09:08:23 +0200

    Add version and toolchain to img filename (#18)
    * add version and toolchain to img filename
    ---------
    
    Co-authored-by: Apaczer <94932128+Apaczer@users.noreply.github.com>

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
+export IMAGE_NAME=${2}
 STARTDIR=`pwd`
 SELFDIR=`dirname \`realpath ${0}\``
 
 cp -r board/miyoo/boot "${BINARIES_DIR}"
 cp -r board/miyoo/main "${BINARIES_DIR}"
-
 support/scripts/genimage.sh ${1} -c board/miyoo/genimage-sdcard.cfg
