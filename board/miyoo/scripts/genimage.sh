a2f39f8c83d36330bb638e39ceed8345f00cfd50
tiopex <tiopxyz@gmail.com>
2023-03-27 09:55:45 +0200

    Generate sdk tarball file by buildroot Fix gmenu2x output files when rebuild image

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -6,4 +6,6 @@ SELFDIR=`dirname \`realpath ${0}\``
 
 cp -r board/miyoo/boot "${BINARIES_DIR}"
 cp -r board/miyoo/main "${BINARIES_DIR}"
+mkdir -p "${BINARIES_DIR}/gmenu2x"
+cp -r "${BINARIES_DIR}/gmenu2x" "${BINARIES_DIR}/main/"
 support/scripts/genimage.sh ${1} -c board/miyoo/genimage-sdcard.cfg
