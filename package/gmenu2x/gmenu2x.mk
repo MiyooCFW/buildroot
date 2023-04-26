66ff4de1eaea29e2fbe2c9f959a6feaf19185e3e
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-04-26 19:08:59 +0200

    [GMENU2X] updt BUILDTIME macro + check if CFW_HASH exist && neq BUILDROOT_HASH (#33)
    * Update BUILDTIME var. in gmenu2x.mk
    
    * check if CFW_HASH =/ BUILDROOT_HASH in gmenu2x.mk

--- package/gmenu2x/gmenu2x.mk
+++ package/gmenu2x/gmenu2x.mk
@@ -4,12 +4,14 @@ GMENU2X_SITE = https://github.com/MiyooCFW/gmenu2x.git
 GMENU2X_DEPENDENCIES = sdl sdl_image sdl_mixer sdl_sound sdl_ttf
 
 PLATFORM = miyoo
-BUILDTIME := $(shell date +%s)
+BUILDTIME := \"$(shell date +%F\ %H:%M)\"
 BUILDROOT_HASH := $(shell git rev-parse --short HEAD)
 SDL_CFLAGS  = $(shell $(STAGING_DIR)/usr/bin/sdl-config --cflags)
 CFLAGS = -DPLATFORM=\"$(PLATFORM)\" -D__BUILDTIME__=$(BUILDTIME) -D__BUILDROOT_HASH__=$(BUILDROOT_HASH) -DLOG_LEVEL=3
 ifdef CFW_HASH
+ifneq ($(CFW_HASH), $(BUILDROOT_HASH)) 
 CFLAGS += -D__CFW_HASH__=$(CFW_HASH)
+endif
 endif
 CFLAGS += -Isrc 
 CFLAGS += -O0 -ggdb -g3 $(SDL_CFLAGS)
