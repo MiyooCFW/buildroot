2aaffcfa2780b85eacbb04b2dca26856da24f870
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-05-16 22:37:26 +0200

    [BOARD] update st7789sfb video driver long initialization fix

--- board/miyoo/boot/normalboot.custom.sh
+++ board/miyoo/boot/normalboot.custom.sh
@@ -1,5 +1,7 @@
 # expected to be run from this directory
 # LOGS variable is set, but it should usually be /dev/null
 # CONSOLE_VARIANT is also set
-#
+# launch daemon
 daemon >> "${LOGS}" 2>&1
+# st7789sfb video driver long initialization fix
+echo " "
