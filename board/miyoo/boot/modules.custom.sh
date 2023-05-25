7ab070a4c9750e85fdf7ede9e4ecfc5366048318
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-05-25 21:44:51 +0200

    [BOARD] add modules.custom script

---
+++ board/miyoo/boot/modules.custom.sh
@@ -0,0 +1,9 @@
+# Leave one line uncommented to load expected video driver
+#
+modprobe r61520fb flip=1
+#modprobe r61520fb debug=1
+#modprobe st7789sfb lowcurrent=1 tefix=0
+#modprobe st7789sTEfb
+#modprobe rm68090fb
+#modprobe gc9306fb
+#modprobe hx8347dfb
