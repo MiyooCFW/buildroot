53ef6c844a015d09e9c69f922a90aeb879bcedf2
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-10-26 17:49:10 +0200

    update ffmpeg_record.sh script

--- board/miyoo/main/apps/ffmpeg/ffmpeg_record.sh
+++ board/miyoo/main/apps/ffmpeg/ffmpeg_record.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-mkdir -p /mnt/output
-
 if pgrep "ffmpeg" > /dev/null; then
     killall -2 ffmpeg
+    while (pgrep "ffmpeg"); do
+        sleep 1
+    done
 else
-    ffmpeg -f fbdev -framerate 15 -i /dev/fb0 -vf "format=yuv420p" -c:v mpeg4 "/mnt/output/$(date +%Y%m%d%H%M%S).mp4" -nostdin &
+    mkdir -p /mnt/output
+    ffmpeg -f fbdev -r 13 -i /dev/fb0 -c:v mpeg4 "/mnt/output/$(date +%Y%m%d%H%M%S).mp4" -nostdin &
 fi
