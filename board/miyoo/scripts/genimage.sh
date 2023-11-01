56b734ed07a6da5e8eb6c7b7e3f51b194fb17684
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-11-01 15:29:35 +0100

    [GENIMAGE] write CFW hash to miyoo splash

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -8,5 +8,5 @@ cp -r board/miyoo/boot "${BINARIES_DIR}"
 cp -r board/miyoo/main "${BINARIES_DIR}"
 mkdir -p "${BINARIES_DIR}/gmenu2x"
 cp -r "${BINARIES_DIR}/gmenu2x" "${BINARIES_DIR}/main/"
-convert board/miyoo/miyoo-boot.png -pointsize 12 -fill white -annotate +10+230 "v2.0.0-"`git rev-parse --short HEAD` -alpha off -type truecolor -strip -define bmp:format=bmp4 -define bmp:subtype=RGB565 "${BINARIES_DIR}"/boot/miyoo-boot.bmp
+convert board/miyoo/miyoo-boot.png -pointsize 12 -fill white -annotate +10+230 "v2.0.0-CFW=${CFW_HASH}-B2R="`git rev-parse --short HEAD` -alpha off -type truecolor -strip -define bmp:format=bmp4 -define bmp:subtype=RGB565 "${BINARIES_DIR}"/boot/miyoo-boot.bmp
 support/scripts/genimage.sh ${1} -c board/miyoo/genimage-sdcard.cfg
