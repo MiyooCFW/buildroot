feb452d9ff0e0c92a983a52100b826c3ebbadb6b
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-11-01 09:10:14 +0100

    inline name of cmd and script dir

--- board/miyoo/main/apps/ffmpeg/video_record.sh
+++ board/miyoo/main/apps/ffmpeg/video_record.sh
@@ -3,5 +3,5 @@ if pgrep "ffmpeg" > /dev/null; then
 	pkill ffmpeg
 else
 	mkdir -p /mnt/output
-	ffmpeg -f fbdev -r 13 -i /dev/fb0 -c:v mpeg4 "/mnt/output/video_$(date +%Y%m%d%H%M%S).mp4" -nostdin
+	ffmpeg -f fbdev -r 8 -i /dev/fb0 -c:v mpeg4 -framerate 15 "/mnt/output/video_$(date +%Y%m%d%H%M%S).mp4" -nostdin
 fi
