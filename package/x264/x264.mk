53a90aa5372112c077d6a5956b5bbff604f666ad
tiopex <tiopxyz@gmail.com>
2023-11-07 14:43:28 +0100

    change repo type from git to tar.gz for x264, due to very long clone git repo

--- package/x264/x264.mk
+++ package/x264/x264.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 X264_VERSION = 35417dcd65a57321fbadf98c9a4cff1cb741db4b
-X264_SITE = https://code.videolan.org/videolan/x264.git
-X264_SITE_METHOD = git
+X264_SITE = https://code.videolan.org/videolan/x264/-/archive/$(VERSION)/x264-$(VERSION).tar.gz
 X264_LICENSE = GPL-2.0+
 X264_DEPENDENCIES = host-pkgconf
 X264_LICENSE_FILES = COPYING
