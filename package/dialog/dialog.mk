c4cd43c67bdf36903ba9a9c3ac3b9144041cae2b
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-11-26 20:05:29 +0100

    [PACKAGE] change DIALOG dl method + add wchar support

--- package/dialog/dialog.mk
+++ package/dialog/dialog.mk
@@ -4,11 +4,8 @@
 #
 ################################################################################
 
-DIALOG_VERSION = 1.3-20230209
-DIALOG_SOURCE = dialog-$(DIALOG_VERSION).tgz
-DIALOG_SITE = https://invisible-mirror.net/archives/dialog
-DIALOG_CONF_OPTS = --with-ncurses --with-curses-dir=$(STAGING_DIR)/usr \
-	--disable-rpath-hack
+DIALOG_VERSION = 20230209
+DIALOG_SITE = $(call github,ThomasDickey,dialog-snapshots,t$(DIALOG_VERSION))
 DIALOG_DEPENDENCIES = host-pkgconf ncurses $(TARGET_NLS_DEPENDENCIES)
 DIALOG_LICENSE = LGPL-2.1
 DIALOG_LICENSE_FILES = COPYING
@@ -17,6 +14,14 @@ ifneq ($(BR2_ENABLE_LOCALE),y)
 DIALOG_DEPENDENCIES += libiconv
 endif
 
+ifneq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
+DIALOG_CONF_OPTS = --with-ncurses
+else
+DIALOG_CONF_OPTS = --with-ncursesw --enable-widec
+endif
+
+DIALOG_CONF_OPTS += --with-curses-dir=$(STAGING_DIR)/usr \
+	--disable-rpath-hack
 DIALOG_CONF_OPTS += NCURSES_CONFIG=$(STAGING_DIR)/usr/bin/$(NCURSES_CONFIG_SCRIPTS)
 
 $(eval $(autotools-package))
