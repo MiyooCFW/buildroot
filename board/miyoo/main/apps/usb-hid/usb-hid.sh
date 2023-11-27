0a221cc5a145fb12dd74f1be78da5b922da129d0
tiopex <tiopxyz@gmail.com>
2023-11-27 10:43:03 +0100

    use st terminal to execute script

--- board/miyoo/main/apps/usb-hid/usb-hid.sh
+++ board/miyoo/main/apps/usb-hid/usb-hid.sh
@@ -1,12 +1,4 @@
 #!/bin/busybox sh
-if [ `cat /sys/devices/platform/soc/1c13000.usb/musb-hdrc.1.auto/mode` != "b_peripheral" ]; then
-	echo -e "\e[31mFirst connect handheld to device!\e[0m"&>$(tty)
-	sleep 2
-	exit
-fi
+/usr/bin/st -k -e /mnt/apps/usb-hid/_usb-hid.sh
 gadget-vid-pid-remove 0x1d6b:0x0104
-gadget-hid
-echo -e "\e[32mStarting USB HID mode...\e[0m"&>$(tty)
-python /mnt/apps/usb-hid/usb-hid.py&>$(tty)
-gadget-vid-pid-remove 0x1d6b:0x0105
 gadget-ms /dev/mmcblk0p1 /dev/mmcblk0p4
