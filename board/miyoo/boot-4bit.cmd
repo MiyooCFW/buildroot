2f89873479b00e4fa102b84459d33a12871502c6
tiopex <67048640+tiopex@users.noreply.github.com>
2023-11-30 11:05:17 +0100

    [BOARD/CONFIG]fix dtb names (#62)

--- board/miyoo/boot-4bit.cmd
+++ board/miyoo/boot-4bit.cmd
@@ -1,6 +1,6 @@
 fatload mmc 0:1 0x80008000 uEnv.txt
 env import -t 0x80008000 ${filesize}
 run bootcmd_args
-load mmc 0:1 0x80C00000 suniv-f1c500s-miyoo-4bit.dtb
+load mmc 0:1 0x80C00000 suniv-f1c100s-miyoo-4bit.dtb
 load mmc 0:1 0x80008000 zImage
 bootz 0x80008000 - 0x80C00000
