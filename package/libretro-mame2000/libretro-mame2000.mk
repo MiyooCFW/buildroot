e711974e891034cef4e3e685a297e29660917fa5
tiopex <tiopxyz@gmail.com>
2024-01-10 11:22:51 +0100

    Optimize retroarch cores: fceumm, mame2000, snes9x2002, tyrquake, prboom

--- package/libretro-mame2000/libretro-mame2000.mk
+++ package/libretro-mame2000/libretro-mame2000.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Commit of 2023/04/25
-LIBRETRO_MAME2000_VERSION = 720b8ad4cbd76abd57b9aeced9ba541dc8476f7f
+# Commit of 2023/11/01
+LIBRETRO_MAME2000_VERSION = 1472da3a39ab14fff8325b1f51a1dfdb8eabb5c8
 LIBRETRO_MAME2000_SITE = $(call github,libretro,mame2000-libretro,$(LIBRETRO_MAME2000_VERSION))
 LIBRETRO_MAME2000_LICENSE = MAME
 LIBRETRO_MAME2000_NON_COMMERCIAL = y
