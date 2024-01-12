c0e05d10be68ef61b59bf4fdc33ff3c0625b1df2
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-01-12 20:55:53 +0100

    [GENIMAGE/ACTIONS] revert ver. to 2.0.0-BETAv2
    + append auto-builds with "Dev_build" string

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-CFW_RELEASE="2.0.1"
-STATUS="BETA"
+CFW_RELEASE="2.0.0"
+STATUS="BETAv2"
 BR2_VENDOR=${2}
 BR2_VERSION_FULL=${3}
 LIBC=${4}
-export IMAGE_NAME="${BR2_VENDOR}-cfw-${CFW_RELEASE}${BR2_VERSION_FULL}_${LIBC}-${STATUS}.img"
+export IMAGE_NAME="${BR2_VENDOR}-cfw-${CFW_RELEASE}${BR2_VERSION_FULL}_${LIBC}-${STATUS}${APPEND_VERSION}.img"
 STARTDIR=`pwd`
 SELFDIR=`dirname \`realpath ${0}\``
 
@@ -46,7 +46,7 @@ else
 fi
 
 # Write CFW version to splash image
-convert board/miyoo/miyoo-splash.png -pointsize 12 -fill white -annotate +10+230 "v${CFW_RELEASE} ${CFW_VERSION} (${LIBC}) ${STATUS}" -type Palette -colors 224 -depth 8 -compress none -verbose BMP3:"${BINARIES_DIR}"/boot/miyoo-splash.bmp
+convert board/miyoo/miyoo-splash.png -pointsize 12 -fill white -annotate +10+230 "v${CFW_RELEASE} ${CFW_VERSION} (${LIBC}) ${STATUS}${APPEND_VERSION}" -type Palette -colors 224 -depth 8 -compress none -verbose BMP3:"${BINARIES_DIR}"/boot/miyoo-splash.bmp
 
 # Generate MAIN BTRFS partition
 image="${BINARIES_DIR}/main.img"
