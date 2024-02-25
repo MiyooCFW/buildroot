5c1e9aaeb72ab0c676d00dd050cdfd7b41abf1e0
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-02-25 12:12:18 +0100

    BOARD: clean naming of profiling data *.gcda & optionize PGO (#92)
    * [BOARD] don't use "preserve-paths" in srcname of profiling *.gcda data
     update PGO optimized libretro cores to not use `-fprofile-dir=path`
    
    * [PACKAGE] optionize PGO optimized builds
    PROFILE=
    0 - don't use PGO
    YES - generate data at runtime
    APPLY - apply optimization at compilation step

--- package/libretro-gpsp/libretro-gpsp.mk
+++ package/libretro-gpsp/libretro-gpsp.mk
@@ -9,11 +9,19 @@ LIBRETRO_GPSP_VERSION = 092c5144827ee3ac07a6af9276244221c303ca41
 LIBRETRO_GPSP_SITE = $(call github,libretro,gpsp,$(LIBRETRO_GPSP_VERSION))
 LIBRETRO_GPSP_LICENSE = GPL-2.0
 LIBRETRO_GPSP_LICENSE_FILES = COPYING
+# Optimize build with Profile Guided Optimization (values: 0, YES, APPLY)
+LIBRETRO_GPSP_PGO ?= APPLY
+
+ifeq ($(LIBRETRO_GPSP_PGO), APPLY)
+define LIBRETRO_GPSP_CONFIGURE_CMDS
+	cp -r $(BASE_DIR)/../board/miyoo/profile/libretro-gpsp/* $(@D)/
+endef
+endif
 
 define LIBRETRO_GPSP_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO) -Ofast -fdata-sections -ffunction-sections -fsingle-precision-constant -flto" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(RETROARCH_LIBRETRO_PLATFORM)" OPTIMIZE="$(COMPILER_COMMONS_LDFLAGS_SO)"
+		$(MAKE) PROFILE="$(LIBRETRO_GPSP_PGO)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(RETROARCH_LIBRETRO_PLATFORM)" OPTIMIZE="$(COMPILER_COMMONS_LDFLAGS_SO)"
 		$(TARGET_STRIP) --strip-unneeded $(@D)/*_libretro.so
 endef
 
