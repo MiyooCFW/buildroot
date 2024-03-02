4ecf594afb1d7a40efc2a39af06042ba5264b11e
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-03-02 22:16:28 +0100

    [GENIMAGE]  automate CFW release tag & status ver.
    Update build.yml:
    - allow for manual workflow dispatch
    - fetch-depth=0 (can't fetch-tags)
    - rm commented line
    - cleanup

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 set -e
-CFW_RELEASE="2.0.0"
-STATUS="BETAv2"
+
+# Generate CFW release tag, status and append iteration count
+if test $(git tag | wc -l) -ne 0; then
+	GIT_TAG="$(git describe --tags --abbrev=0)"
+	CFW_RELEASE="$(echo ${GIT_TAG} | sed 's/-.*//')"
+	STATUS="$(echo ${GIT_TAG} | sed 's/^[^-]*-//' | tr '[:lower:]' '[:upper:]' | tr '-' 'v')"
+	ITERATION_VERSION="$(git rev-list --count ${GIT_TAG}..HEAD)"
+	if test $ITERATION_VERSION -eq 0; then
+			APPEND_VERSION=""
+			if test "$STATUS" == "$CFW_RELEASE"; then STATUS="STABLE"; fi
+	else
+			if [[ "$(echo ${STATUS} | sed 's/^[^-]*v//')" =~ ^-?[0-9]+$ ]]; then
+					APPEND_VERSION="v$(echo ${STATUS} | sed 's/^[^-]*v//' | expr $(cat -) + 1)${APPEND_VERSION}"
+					STATUS="$(echo ${STATUS} | sed 's/v.*//')"
+			elif [ "$STATUS" == "BETA" ]; then
+					STATUS="${STATUS}v2"
+			fi
+			APPEND_VERSION="${APPEND_VERSION}-n${ITERATION_VERSION}"
+			if test "$STATUS" == "$CFW_RELEASE"; then STATUS="BETA"; fi
+	fi
+else
+	CFW_RELEASE="0.0.0"
+	STATUS="UNKNOWN"
+fi
+
+# Output Image name
 BR2_VENDOR=${2}
 BR2_VERSION_FULL=${3}
 LIBC=${4}
 export IMAGE_NAME="${BR2_VENDOR}-cfw-${CFW_RELEASE}${BR2_VERSION_FULL}_${LIBC}-${STATUS}${APPEND_VERSION}.img"
+
 STARTDIR=`pwd`
 SELFDIR=`dirname \`realpath ${0}\``
 
@@ -16,7 +41,7 @@ cp -r board/miyoo/main "${BINARIES_DIR}"
 # Workaround for build apss and configs being placed in /usr/ after img generation (as we use MAIN)
 test -d "${BINARIES_DIR}/gmenu2x" && cp -r "${BINARIES_DIR}/gmenu2x/" "${BINARIES_DIR}/main/"
 test -d "${BINARIES_DIR}/emus" && cp -r "${BINARIES_DIR}/emus/" "${BINARIES_DIR}/main/"
-if test -d "${BINARIES_DIR}/retroarch";then
+if test -d "${BINARIES_DIR}/retroarch"; then
 	rsync -avzh "${BINARIES_DIR}/retroarch/" "${BINARIES_DIR}/main/.retroarch/"
 	## Generate list of cores to be used
 	CORES_DIR="${BINARIES_DIR}/retroarch/cores"
