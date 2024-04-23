d7324d3e833f018f52e3b39eacd5fe7b75099227
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-04-23 22:09:35 +0200

    BOARD: update Video Record app
    Update video_record.sh

--- board/miyoo/main/apps/ffmpeg/video_record.sh
+++ board/miyoo/main/apps/ffmpeg/video_record.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+st_exec_func(){
+	st -k -e "/bin/sh" "-c" "${1}"
+}
+
+OUTPUTDIR="/mnt/output"
+
 if pgrep "ffmpeg" > /dev/null; then
-	pkill ffmpeg
+	st_exec_func "pkill ffmpeg >/dev/null;\
+	 echo -e \"\n\n\n\n\n\n\t\t\tExiting VIDEO recording!\nWait a minute to end writing MP4 file to ${OUTPUTDIR}/\";\
+	 while pgrep ffmpeg >/dev/null; do sleep 1; done"
 else
-	mkdir -p /mnt/output
-	ffmpeg -f fbdev -r 8 -i /dev/fb0 -c:v mpeg4 -framerate 15 "/mnt/output/video_$(date +%Y%m%d%H%M%S).mp4" -nostdin
+	mkdir -p ${OUTPUTDIR}
+	ffmpeg -f fbdev -r 10 -i /dev/fb0 -vf "format=yuv420p"\
+	 -c:v libx264 -preset ultrafast -framerate 10 "${OUTPUTDIR}/video_$(date +%Y%m%d%H%M%S).mp4" -nostdin -loglevel warning &
+	# Wait a few seconds before actual screen record, the X264 encoder needs resources before it will stabilize video ouput
+	st_exec_func "echo -e \"\n\n\n\n\n\n\t\t\tStarting VIDEO record...\n\t\t\tWait a few seconds to start.\"; read -t 10"
 fi
