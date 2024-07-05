452f493e75ed45d11c6e95fb21ba6fbeca58dafd
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-07-05 23:01:30 +0200

    PACKAGE: add new `gmenu2x-gm2xpkg` package to SDK (#113)
    - install `gm2xpkg` to SDK
    - add new GMENU2X_GM2XPKG package
    - dl single file source of raw.githubusercontent.com

---
+++ package/gmenu2x-gm2xpkg/gmenu2x-gm2xpkg.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# GMenu2X's "gm2xpkg" script
+#
+################################################################################
+
+# Not a BR2_PACKAGE_FOO_VERSION
+GM2XPKG_VERSION = master
+
+GMENU2X_GM2XPKG_EXTRA_DOWNLOADS = https://raw.githubusercontent.com/MiyooCFW/gmenu2x/$(GM2XPKG_VERSION)/tools/gm2xpkg.sh
+GMENU2X_GM2XPKG_INSTALL_STAGING = YES
+GMENU2X_GM2XPKG_INSTALL_TARGET = NO
+
+# Install GMenu2X PacKaGer to staging for development
+define GMENU2X_GM2XPKG_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(GMENU2X_GM2XPKG_DL_DIR)/gm2xpkg.sh $(STAGING_DIR)/usr/bin/gm2xpkg
+endef
+
+$(eval $(generic-package))
