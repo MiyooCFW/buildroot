458461ffbfd52aaa11a9a388d6552a0346762609
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-08-07 15:50:42 +0200

    PACKAGE-sdl_ttf: bump version to 23-Apr-2024

--- package/sdl_ttf/sdl_ttf.mk
+++ package/sdl_ttf/sdl_ttf.mk
@@ -6,7 +6,9 @@
 
 # There is unlikely to be a new SDL_ttf release for the foreseeable future:
 # https://bugzilla.libsdl.org/show_bug.cgi?id=5344#c1
-SDL_TTF_VERSION = 70b2940cc75e92aab02a67d2f827caf2836a2c74
+#
+# This commit points to the SDL-1.2 branch from 23 Apr 2024.
+SDL_TTF_VERSION = 3c4233732b94ce08d5f6a868e597af39e13f8b23
 SDL_TTF_SITE = $(call github,libsdl-org,SDL_ttf,$(SDL_TTF_VERSION))
 SDL_TTF_LICENSE = Zlib
 SDL_TTF_LICENSE_FILES = COPYING
