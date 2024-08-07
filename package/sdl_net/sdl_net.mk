eafadce37351b6eb3eaac5c433fce79f63385eb0
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-08-07 21:26:16 +0200

    PACKAGE-sdl_net/sdl_ttf/sdl_image: bump version to 23-Apr-2024 (#118)

--- package/sdl_net/sdl_net.mk
+++ package/sdl_net/sdl_net.mk
@@ -7,8 +7,8 @@
 # The latest officially released version of SDL_image is 1.2.8, released in 2012.
 # Since then, there have been several bugfixes.
 #
-# This commit points to the SDL-1.2 branch from 18 Feb 2021.
-SDL_NET_VERSION = 620b0ba7dd84a0fdbd4cc8ef1b2be1cc10f90ae3
+# This commit points to the SDL-1.2 branch from 23 Apr 2024.
+SDL_NET_VERSION = 0043be2e559f8d562d04bf62d6e3f4162ed8edad
 SDL_NET_SITE = $(call github,libsdl-org,SDL_net,$(SDL_NET_VERSION))
 SDL_NET_SOURCE = SDL_net-$(SDL_NET_VERSION).tar.gz
 SDL_NET_LICENSE = Zlib
