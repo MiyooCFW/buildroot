3eaaba588f6a1510272f7b87988dbf3118410459
tiopex <67048640+tiopex@users.noreply.github.com>
2024-10-09 02:08:42 +0200

    Add binary emus: DrPocketSNES, FCEUX, GNGEO, gpSP, RACE, PCSX_reARMed, Gambatte (#127)

---
+++ package/ipk-gngeo/ipk-gngeo.mk
@@ -0,0 +1,15 @@
+IPK_GNGEO_VERSION = v20210731-m1
+IPK_GNGEO_SITE = https://github.com/Apaczer/gngeo/releases/download/$(IPK_GNGEO_VERSION)
+IPK_GNGEO_SOURCE = gngeo.ipk
+IPK_GNGEO_INSTALL_TARGET = YES
+
+define IPK_GNGEO_EXTRACT_CMDS
+    cd $(@D) && ar x $(DL_DIR)/ipk-gngeo/$(IPK_GNGEO_SOURCE)
+endef
+
+define IPK_GNGEO_INSTALL_TARGET_CMDS
+    mkdir -p $(BINARIES_DIR)/main
+    tar -xzf $(@D)/data.tar.gz --strip-components=2 -C $(BINARIES_DIR)/main
+endef
+
+$(eval $(generic-package))
---
+++ package/ipk-pcsx_rearmed/ipk-pcsx_rearmed.mk
@@ -0,0 +1,15 @@
+IPK_PCSX_REARMED_VERSION = 1.0
+IPK_PCSX_REARMED_SITE = https://github.com/tiopex/pcsx_rearmed/releases/download/$(IPK_PCSX_REARMED_VERSION)
+IPK_PCSX_REARMED_SOURCE = pcsx.ipk
+IPK_PCSX_REARMED_INSTALL_TARGET = YES
+
+define IPK_PCSX_REARMED_EXTRACT_CMDS
+    cd $(@D) && ar x $(DL_DIR)/ipk-pcsx_rearmed/$(IPK_PCSX_REARMED_SOURCE)
+endef
+
+define IPK_PCSX_REARMED_INSTALL_TARGET_CMDS
+    mkdir -p $(BINARIES_DIR)/main
+    tar -xzf $(@D)/data.tar.gz --strip-components=2 -C $(BINARIES_DIR)/main
+endef
+
+$(eval $(generic-package))
