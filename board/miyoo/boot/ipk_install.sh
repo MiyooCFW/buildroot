8e854946a6cd2fb88a6113bf408b4d9342326696
TriForce <triforce@gznetwork.com>
2024-11-22 19:15:01 -0300

    Update texts on ipk manager

--- board/miyoo/boot/ipk_install.sh
+++ board/miyoo/boot/ipk_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export TERM=linux
+export HELP="Info"
 WARN="
 MiyooCFW team is not responsible for any issues \
 arising from the use of third-party applications \
@@ -31,7 +32,7 @@ if [[ ${#package_list[@]} -eq 0 ]]; then
 fi
 
 dialog --backtitle "MiyooCFW 2.0" --timeout 10 --title "IPK package manager" --colors --no-shadow --msgbox "\nSelect pkg with \Zb\Z3Y\Zn button on the list." 8 28
-until selected_packages=$(dialog --backtitle "MiyooCFW 2.0" --title "IPK package manager" --colors --no-shadow --help-button --help-status --checklist "Press \Zb\Z3Y\Zn to select packages to install:" 20 60 15 "${package_list[@]}" 3>&1 1>&2 2>&3); do
+until selected_packages=$(dialog --backtitle "MiyooCFW 2.0" --title "IPK package manager" --colors --no-shadow --help-button --help-status --checklist "Press \Zb\Z3Y\Zn to select packages (non RetroArch) to install:" 20 60 15 "${package_list[@]}" 3>&1 1>&2 2>&3); do
     case $? in
 	(1) btn=Cancel && break;;
 	(2)
