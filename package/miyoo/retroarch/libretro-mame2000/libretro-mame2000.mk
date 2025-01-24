e3d39f3e87a630cdafd9fdfc8d0ae0d032e414e4
Apaczer <94932128+Apaczer@users.noreply.github.com>
2025-01-24 08:24:01 +0100

    PACKAGE: bumpup ver., fix SELECT input & reoptimize `libretro-mame2000` (#146)
    * PACKAGE: bump libretro-mame2000 version
    
    commit 2ec60f6e1078cf9ba173e80432cc28fd4eea200f
    
    * PACKAGE-mame2000: fix SELECT btn & optimize patches

--- package/miyoo/retroarch/libretro-mame2000/libretro-mame2000.mk
+++ package/miyoo/retroarch/libretro-mame2000/libretro-mame2000.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Commit of 2023/11/01
-LIBRETRO_MAME2000_VERSION = 1472da3a39ab14fff8325b1f51a1dfdb8eabb5c8
+# Commit of Jul 1, 2024
+LIBRETRO_MAME2000_VERSION = 2ec60f6e1078cf9ba173e80432cc28fd4eea200f
 LIBRETRO_MAME2000_SITE = $(call github,libretro,mame2000-libretro,$(LIBRETRO_MAME2000_VERSION))
 LIBRETRO_MAME2000_LICENSE = MAME
 LIBRETRO_MAME2000_LICENSE_FILES = readme.txt
@@ -18,7 +18,7 @@ define LIBRETRO_MAME2000_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CC="$(TARGET_CC)" -C $(@D) -f Makefile ARM=1 USE_CYCLONE=1 USE_DRZ80=1 platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+		$(MAKE) CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)"
 		$(TARGET_STRIP) --strip-unneeded $(@D)/*_libretro.so
 endef
 
