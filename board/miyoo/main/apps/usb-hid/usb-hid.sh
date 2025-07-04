5363a734e6f7c3da1ba5288cfd3907b2b4ee4ca7
Apaczer <94932128+Apaczer@users.noreply.github.com>
2025-07-04 21:55:29 +0200

    firstboot: correct btrfs loading only on fs change

--- board/miyoo/main/apps/usb-hid/usb-hid.sh
+++ board/miyoo/main/apps/usb-hid/usb-hid.sh
@@ -5,20 +5,19 @@ st_error_func(){
 st_exec_func(){
 	st -k -e "/bin/sh" "-c" "${1}"
 }
-	echo peripheral > /sys/devices/platform/soc/1c13000.usb/musb-hdrc.1.auto/mode
-	killall umtprd umtprd-debug
-	/mnt/apps/usb-mtd/remove.sh g2
-	modprobe usb_f_hid
-	modprobe evdev
-	modprobe uinput
-	mount none /sys/kernel/config -t configfs
-	gadget-hid
-	st_exec_func "\
+echo peripheral > /sys/devices/platform/soc/1c13000.usb/musb-hdrc.1.auto/mode
+killall umtprd umtprd-debug
+/mnt/apps/usb-mtd/remove.sh g2
+modprobe usb_f_hid
+modprobe evdev
+modprobe uinput
+mount none /sys/kernel/config -t configfs
+gadget-hid
+st_exec_func "\
 	echo -e \"\e[32m\n\n\n\n\n\n               Starting USB-HID mode\e[0m\n\n\n\"; \
 	sleep 1; \
 	python /mnt/apps/usb-hid/usb-hid.py"
-
-	gadget-vid-pid-remove 0x1d6b:0x0104
-	modprobe -r usb_f_hid
-  modprobe -r evdev
-  modprobe -r uinput
+gadget-vid-pid-remove 0x1d6b:0x0104
+modprobe -r usb_f_hid
+modprobe -r evdev
+modprobe -r uinput
