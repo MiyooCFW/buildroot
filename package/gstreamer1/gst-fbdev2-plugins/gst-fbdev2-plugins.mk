a31292ad4ad37436ee6280523011fc78d3e764c1
tiopex <tiopxyz@gmail.com>
2025-07-10 15:15:14 +0200

    Use HW scaler

--- package/gstreamer1/gst-fbdev2-plugins/gst-fbdev2-plugins.mk
+++ package/gstreamer1/gst-fbdev2-plugins/gst-fbdev2-plugins.mk
@@ -9,7 +9,7 @@ GST_FBDEV2_PLUGINS_SITE = $(call github,hglm,gst-fbdev2-plugins,$(GST_FBDEV2_PLU
 
 GST_FBDEV2_PLUGINS_LICENSE = LGPL-2.1
 GST_FBDEV2_PLUGINS_LICENSE_FILES = COPYING
-GST_FBDEV2_PLUGINS_DEPENDENCIES = gstreamer1 gst1-plugins-base libglib2 libdrm libtool
+GST_FBDEV2_PLUGINS_DEPENDENCIES = gstreamer1 gst1-plugins-base gst-omx libglib2 libdrm libtool
 GST_FBDEV2_PLUGINS_AUTORECONF = YES
 
 $(eval $(autotools-package))
