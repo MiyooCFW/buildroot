d7e11bdefd85ca20c04b6268d0bce088ad1302a4
tiopex <tiopxyz@gmail.com>
2025-07-12 00:01:40 +0200

    kernel: Add patch to increase VRAM

--- package/gstreamer1/gst-fbdev2-plugins/gst-fbdev2-plugins.mk
+++ package/gstreamer1/gst-fbdev2-plugins/gst-fbdev2-plugins.mk
@@ -11,5 +11,9 @@ GST_FBDEV2_PLUGINS_LICENSE = LGPL-2.1
 GST_FBDEV2_PLUGINS_LICENSE_FILES = COPYING
 GST_FBDEV2_PLUGINS_DEPENDENCIES = gstreamer1 gst1-plugins-base gst-omx libglib2 libdrm libtool
 GST_FBDEV2_PLUGINS_AUTORECONF = YES
+define GST_FBDEV2_PLUGINS_PRE_PATCH_HOOK
+    $(SED) 's/-lgstframebuffersink/libgstframebuffersink.la/g' $(@D)/src/Makefile.am
+endef
+GST_FBDEV2_PLUGINS_PRE_PATCH_HOOKS += GST_FBDEV2_PLUGINS_PRE_PATCH_HOOK
 
 $(eval $(autotools-package))
