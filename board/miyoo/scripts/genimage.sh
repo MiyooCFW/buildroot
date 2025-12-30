13cf60fea4e4a5dd01c16bbe058113813afde1d2
Apaczer <94932128+Apaczer@users.noreply.github.com>
2025-12-30 20:31:12 +0100

    genimage.sh: create RA link in gm2x if not present

--- board/miyoo/scripts/genimage.sh
+++ board/miyoo/scripts/genimage.sh
@@ -83,10 +83,13 @@ if test -d "${BINARIES_DIR}/retroarch"; then
 			touch $RA_WDIR/"${CORE_SCRIPT}"
 			echo -e "#!/bin/sh\n/mnt/emus/retroarch/retroarch -L ${CORE_FILE} \"\$1\" \"\$2\"" > $RA_WDIR/"${CORE_SCRIPT}"
 			chmod +x $RA_WDIR/"${CORE_SCRIPT}"
-			# RA_LDIR="${BINARIES_DIR}/main/gmenu2x/sections/cores"
-			# CORE_LINK="zblank.${CORE_NAME}.ra"
-			# touch $RA_LDIR/"${CORE_LINK}"
-			# echo -e "title=${CORE_NAME}\ndescription=${CORE_NAME} libretro core\nexec=/mnt/emus/retroarch/${CORE_SCRIPT}\nselectordir=/mnt" > $RA_LDIR/"${CORE_LINK}"
+			# sanity check if there's an existing link in gmenu2x
+			RA_LDIR="${BINARIES_DIR}/main/gmenu2x/sections/cores"
+			if ! test -f "${RA_LDIR}"/*".${CORE_NAME}.ra"; then
+				CORE_LINK="zblank.${CORE_NAME}.ra"
+				touch "${RA_LDIR}"/"${CORE_LINK}"
+				echo -e "title=${CORE_NAME}\ndescription=${CORE_NAME} libretro core\nexec=/mnt/emus/retroarch/${CORE_SCRIPT}\nselectordir=/mnt" > "${RA_LDIR}"/"${CORE_LINK}"
+			fi
 		fi
 	done
 fi
