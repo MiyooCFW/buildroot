1836c95e31c0212547f0fafd96a111e955d91add
Apaczer <94932128+Apaczer@users.noreply.github.com>
2026-01-11 18:46:33 +0100

    PKG: libretro-database: fix `rdb` & `cursors` paths
    bum version to bdc93ed8c1e0282665a50aae39d0259b52cdefc9afb37379026c544a2e8aa5b4

--- package/miyoo/retroarch/libretro-database/libretro-database.mk
+++ package/miyoo/retroarch/libretro-database/libretro-database.mk
@@ -1,14 +1,14 @@
-LIBRETRO_DATABASE_VERSION = f8100512f424873a549bb07ab7ff9f300fab8adf
+LIBRETRO_DATABASE_VERSION = fbcc8c1c24d8b20b6aaca95b4da6a2f39ad85f05
 LIBRETRO_DATABASE_SITE = $(call github,libretro,libretro-database,${LIBRETRO_DATABASE_VERSION})
 LIBRETRO_DATABASE_LICENSE = CC-BY-SA-4.0
 LIBRETRO_DATABASE_LICENSE_FILES = LICENSE
 
 define LIBRETRO_DATABASE_INSTALL_TARGET_CMDS
     mkdir -p "${BINARIES_DIR}/retroarch/cores"
-        mkdir -p "${BINARIES_DIR}/retroarch/cursors"
-        mkdir -p "${BINARIES_DIR}/retroarch/rdb"
-        $(INSTALL) -D -m 0644  $(@D)/cursors/* "${BINARIES_DIR}/retroarch/cursors/"
-	$(INSTALL) -D -m 0644 $(@D)/rdb/* "${BINARIES_DIR}/retroarch/rdb/"
+        mkdir -p "${BINARIES_DIR}/retroarch/database/cursors"
+        mkdir -p "${BINARIES_DIR}/retroarch/database/rdb"
+        $(INSTALL) -D -m 0644  $(@D)/cursors/* "${BINARIES_DIR}/retroarch/database/cursors/"
+	$(INSTALL) -D -m 0644 $(@D)/rdb/* "${BINARIES_DIR}/retroarch/database/rdb/"
 endef
 
 $(eval $(generic-package))
