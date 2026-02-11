e7a150d8c59136c7976854ab6a945feee4e3990e
Apaczer <94932128+Apaczer@users.noreply.github.com>
2026-02-12 00:03:56 +0100

    PKG: lr-ecwolf: fix deprecated submodule failure (#194)
    use LIBFOO extra downloads

--- package/miyoo/retroarch/libretro-ecwolf/libretro-ecwolf.mk
+++ package/miyoo/retroarch/libretro-ecwolf/libretro-ecwolf.mk
@@ -5,12 +5,22 @@
 ################################################################################
 
 LIBRETRO_ECWOLF_VERSION = 18eca17c2d634b154824e0782c6cbbe0a2c9ea76
+LIBRETRO_ECWOLF_SUBMODULE_VERSION = 996376e36d3f4f56eba202cb96230568628d2583
 LIBRETRO_ECWOLF_SITE = https://github.com/libretro/ecwolf.git
-LIBRETRO_ECWOLF_SITE_METHOD=git
-LIBRETRO_ECWOLF_GIT_SUBMODULES=y
+LIBRETRO_ECWOLF_SITE_METHOD = git
+LIBRETRO_ECWOLF_EXTRA_DOWNLOADS = \
+	$(call github,libretro,libretro-common,$(LIBRETRO_ECWOLF_SUBMODULE_VERSION))/libretro-libretro-common-$(LIBRETRO_ECWOLF_SUBMODULE_VERSION).tar.gz
+#LIBRETRO_ECWOLF_GIT_SUBMODULES=y
 LIBRETRO_ECWOLF_LICENSE = GPL-2.0+, ID-Software, MAME, Other
 LIBRETRO_ECWOLF_LICENSE_FILES = docs/copyright docs/license-gpl.txt docs/license-id.txt docs/license-mame.txt
 LIBRETRO_ECWOLF_NON_COMMERCIAL = y
+
+define LIBRETRO_ECWOLF_POST_EXTRACT_SUBMODULE_FIXUP
+	mkdir -p $(@D)/src/libretro/libretro-common
+	tar xvf $(LIBRETRO_ECWOLF_DL_DIR)/libretro-libretro-common-$(LIBRETRO_ECWOLF_SUBMODULE_VERSION).tar.gz --strip-components=1 -C $(@D)/src/libretro/libretro-common
+endef
+
+LIBRETRO_ECWOLF_POST_EXTRACT_HOOKS += LIBRETRO_ECWOLF_POST_EXTRACT_SUBMODULE_FIXUP
 
 define LIBRETRO_ECWOLF_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/src/libretro/Makefile
