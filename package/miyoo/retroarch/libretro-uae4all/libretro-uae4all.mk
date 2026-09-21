61d88bdb7f1a8e767f93461a3722efb02c958b78
Apaczer <94932128+Apaczer@users.noreply.github.com>
2026-09-21 21:47:17 +0200

    PKG: lr-uae4all: bump version & use FAME via make
    - implement savestates
    - optional statusbar
    - should be more compatible
    
    remove unnecessary patch (m68k core type can implied with a flag via make cmd)

--- package/miyoo/retroarch/libretro-uae4all/libretro-uae4all.mk
+++ package/miyoo/retroarch/libretro-uae4all/libretro-uae4all.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-LIBRETRO_UAE4ALL_VERSION = 6419a82126699247462afbaeeed2b730a0cceee4
+LIBRETRO_UAE4ALL_VERSION = b8a2752df1684f9c921ff27a10f15d534189a02c
 LIBRETRO_UAE4ALL_SITE = $(call github,Apaczer,uae4all,$(LIBRETRO_UAE4ALL_VERSION))
-#LIBRETRO_UAE4ALL_BRANCH = libretro
 LIBRETRO_UAE4ALL_LICENSE = GPL-2.0
 LIBRETRO_UAE4ALL_LICENSE_FILES = docs/COPYING
 LIBRETRO_UAE4ALL_DEPENDENCIES = zlib
 
 define LIBRETRO_UAE4ALL_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS)" \
-		$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AS="$(TARGET_AS)" -C $(@D) -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+		$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AS="$(TARGET_AS)" -C $(@D) -f Makefile.libretro \
+			platform="$(RETROARCH_LIBRETRO_PLATFORM)" FAME_CORE=1 FAME_CORE_C=1
 		$(TARGET_STRIP) --strip-unneeded $(@D)/*_libretro.so
 endef
 
